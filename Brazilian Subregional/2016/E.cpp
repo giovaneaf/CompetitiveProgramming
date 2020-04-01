417ba39d03d1359594e916d3e8fae05b96b7aed0
giovaneaf <giovaneaf1995@gmail.com>
2020-04-01 20:03:24 -0300

    finished problem E

--- Brazilian Subregional/2016/E.cpp
+++ Brazilian Subregional/2016/E.cpp
@@ -1,11 +1,3 @@
-/*
-Got TLE on this in URI... I don't know why. I think time limit is so small.
-Tried to implement in O((2^b)*n*|s|) where:
-b is the base (always 16)
-n is the number of strings given (max is 63)
-|s| is the length of the biggest string (max is 9)
-*/
-
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -35,13 +27,19 @@ typedef pair<int, bool> ib;
 int n, k;
 ll fact[25];
 ll power[25];
+ll var[25];
 ll total;
 char str[MAXN][11];
 int v[MAXN][11];
 ll sum[(1 << 16)+5];
 vector<pair<ll, ll>> memo;
-bool used[25];
+bool used[MAXN][20];
+bool seen[(1 << 16)+5];
 int len[MAXN];
+int digitOn[25];
+int digitOff[25];
+int pon, poff;
+char read[5];
 
 inline void init(int n)
 {
@@ -52,6 +50,10 @@ inline void init(int n)
 		fact[i] = (fact[i-1]*i)%MOD;
 		power[i] = power[i-1]*16LL;
 	}
+	FOR(i, 1, 17)
+	{
+		var[i] = (fact[i]*fact[16-i])%MOD;
+	}
 }
 
 inline int toInt(char c)
@@ -60,27 +62,52 @@ inline int toInt(char c)
 	return 10 + c - 'a';
 }
 
-inline ll compute(int mask)
+string toBin(ll x)
 {
-	FOR(i, 0, 16)
+	string s;
+	while(x > 0)
 	{
-		if(mask & (1 << i)) used[i] = true;
-		else used[i] = false;
+		s += (x&1) == 0 ? '0' : '1';
+		x >>= 1;
 	}
+	reverse(s.begin(), s.end());
+	return s;
+}
+
+void compute(int mask, int pos)
+{
 	ll ans;
 	int idx;
 	ans = 0LL;
-	FOR(i, 0, n)
+	idx = 0;
+	RFOR(j, len[pos]-1, 0)
 	{
-		idx = 0;
-		RFOR(j, len[i]-1, 0)
+		if(mask & (1 << v[pos][j])) continue;
+		ans = (ans + power[idx]*v[pos][j]);
+		idx++;
+	}
+	queue<int> q;
+	q.push(mask);
+	seen[mask] = true;
+	int newMask;
+	while(q.size())
+	{
+		mask = q.front();
+		q.pop();
+		sum[mask] += ans;
+		FOR(i, 0, poff)
 		{
-			if(used[v[i][j]]) continue;
-			ans = (ans + power[idx]*v[i][j]);
-			idx++;
+			if((mask & (1 << digitOff[i])) == 0)
+			{
+				newMask = mask | (1 << digitOff[i]);
+				if(!seen[newMask])
+				{
+					seen[newMask] = true;
+					q.push(newMask);
+				}
+			}
 		}
 	}
-	return ans;
 }
 
 pair<ll, ll> solve()
@@ -97,7 +124,7 @@ pair<ll, ll> solve()
 	{
 		mask = q.front().fst; depth = q.front().snd;
 		q.pop();
-		total += (((fact[depth]*fact[16-depth])%MOD)*(sum[mask]%MOD))%MOD; total %= MOD;
+		total += (var[depth]*(sum[mask]%MOD))%MOD; total %= MOD;
 		memo[mask].fst += sum[mask]; memo[mask].snd += sum[mask];
 		FOR(j, 0, 16)
 		{
@@ -127,18 +154,44 @@ pair<ll, ll> solve()
 int main()
 {
 	init(20);
-	while(~scanf("%d", &n))
+	while(~scanf(" %s", &read))
 	{
+		n = stoi(read, NULL, 16);
+		memset(used, false, sizeof(used));
+		memset(sum, 0LL, sizeof(sum));
 		FOR(i, 0, n)
 		{			
 			scanf(" %s", &str[i]);
 			len[i] = strlen(str[i]);
 			FOR(j, 0, len[i])
+			{
 				v[i][j] = toInt(str[i][j]);
+				used[i][v[i][j]] = true;
+			}
 		}
-		FOR(i, 1, (1 << 16))
+		int mask;
+		FOR(i, 0, n)
 		{
-			sum[i] = compute(i);
+			memset(seen, false, sizeof(seen));
+			pon = poff = 0;
+			FOR(j, 0, 16)
+			{
+				if(used[i][j]) digitOn[pon++] = j;
+				else digitOff[poff++] = j;
+			}
+			compute(0, i);
+			FOR(j, 1, (1 << pon))
+			{
+				mask = 0;
+				FOR(k, 0, pon)
+				{
+					if(j & (1 << k))
+					{						
+						mask |= (1 << digitOn[k]);
+					}
+				}
+				compute(mask, i);
+			}
 		}
 		memo.assign((1 << 16 + 5), mp(-1LL, -1LL));
 		total = 0LL;
